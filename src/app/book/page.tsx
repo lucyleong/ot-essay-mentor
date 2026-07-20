ac8cf01a06f6f0fc84b371ff7632c7806709e1d2
Lucy <lucylc@MB-Air-M5-Tahoe-2606.local>
2026-07-20 16:21:44 -0700

    trying again

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -154,10 +154,10 @@ export default function BookPage() {
     }
   }
 
-  function handleAnswerChange(questionId: string, value: any) {
+ function handleAnswerChange(questionId: string, value: any) {
     setAnswers(prev => ({ ...prev, [questionId]: value }))
-const mentorPrevQuestion = questions.find(q => q.question_text === 'I worked with a College Essay Mentor in Spring 2026 through this program')    
-if (mentorPrevQuestion && questionId === mentorPrevQuestion.id) {
+    const mentorPrevQuestion = questions.find(q => q.question_text === 'I worked with a College Essay Mentor in Spring 2026 through this program')
+    if (mentorPrevQuestion && questionId === mentorPrevQuestion.id) {
       setShowMentor(value === 'Yes')
     }
   }
